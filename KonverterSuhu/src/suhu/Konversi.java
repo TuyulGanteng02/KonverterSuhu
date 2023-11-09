5c7785126e2927be35a8d5bf9c798c3fe45bf999
TuyulGanteng02 <147491049+TuyulGanteng02@users.noreply.github.com>
2023-11-10 03:17:03 +0700

    Add files via upload

---
+++ KonverterSuhu/src/suhu/Konversi.java
@@ -0,0 +1,7 @@
+package suhu;
+
+public interface Konversi {
+    public double toCelcius();
+    public double toFahrenheit();
+    public double toKelvin();
+}
---
+++ KonverterSuhu/src/suhu/KonverterSuhu.java
@@ -0,0 +1,8 @@
+package suhu;
+
+public class KonverterSuhu {
+    public static void main(String[] args) {
+        Celcius c = new Celcius("Celcius", 30);
+        c.info();
+    }    
+}
