29a5cdaa937ac750581430e2dd7dc716e7d43653
TuyulGanteng02 <147491049+TuyulGanteng02@users.noreply.github.com>
2023-11-10 13:28:47 +0700

    Update KonverterSuhu.java

--- src/suhu/KonverterSuhu.java
+++ src/suhu/KonverterSuhu.java
@@ -4,5 +4,11 @@ public class KonverterSuhu {
     public static void main(String[] args) {
         Celcius c = new Celcius("Celcius", 30);
         c.info();
+        Reamur r = new Reamur("Reamur", 30);
+        r.info();
+        Fahrenheit f = new Fahrenheit("Fahrenheit", 95);
+        f.info();
+        Kelvin k = new Kelvin("Kelvin", 300.15);
+        k.info();
     }    
 }
